23c8acbb5471cc7a788ba4ec883692f6bbd59ee1
ahmedshekooo <ahmedshekooo2010@gmail.com>
2025-07-23 23:56:07 +0300

    add files

--- script.js
+++ script.js
@@ -17,3 +17,10 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+document.querySelector(".menubtn").addEventListener("click",()=>{
+    document.querySelector(".sidemenu").style.display="block"
+})
+document.querySelector(".close").addEventListener("click",()=>{
+    document.querySelector(".sidemenu").style.display="none"
+
+})
---
+++ search/search.js
@@ -0,0 +1,7 @@
+document.querySelector(".menubtn").addEventListener("click",()=>{
+    document.querySelector(".sidemenu").style.display="block"
+})
+document.querySelector(".close").addEventListener("click",()=>{
+    document.querySelector(".sidemenu").style.display="none"
+
+})
