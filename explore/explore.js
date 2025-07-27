61799b941dc9ce74799e9fc52913832aeb159e0d
ahmedshekooo <ahmedshekooo2010@gmail.com>
2025-07-27 18:25:42 +0300

    add files

--- explore/explore.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".menubtn").addEventListener("click",()=>{
-    document.querySelector(".sidemenu").style.display="block"
-})
-document.querySelector(".close").addEventListener("click",()=>{
-    document.querySelector(".sidemenu").style.display="none"
-
-})
